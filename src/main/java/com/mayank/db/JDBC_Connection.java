5646c53702be1d1d3d54c6b192e4ccb0482cc634
vinayparashar316 <vinayparashar316@gmail.com>
2026-02-02 17:25:31 -0500

    Update JDBC_Connection

--- src/main/java/com/mayank/db/JDBC_Connection.java
+++ src/main/java/com/mayank/db/JDBC_Connection.java
@@ -1,1 +1,32 @@
+package com.mayank.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBC_Connection {
+
+	private static Connection conn;
+
+	public static Connection getConnection() {
+
+		String url = "jdbc:oracle:thin:@localhost:1521:ORCL";
+		String username = "sys as sysdba";
+		String password = "oracle7814";
+
+		try {
+			if (conn == null)
+				conn = DriverManager.getConnection(url, username, password);
+
+			System.out.println("Database connection success");
+
+			return conn;
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return conn;
+		}
+
+	}
+
+}
 
